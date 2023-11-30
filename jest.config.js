6d7c86cb22cbe92e4f78b6ee2f986942253f11e7
s.afshani <s.afshani@khodro45.com>
2023-11-30 17:46:01 +0330

    fix: jest issue

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,19 @@ const createJestConfig = nextJest({
   dir: "./",
 });
 const customJestConfig = {
-  moduleDirectories: ["node_modules", "<rootDir>/"],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: "jest-environment-jsdom",
   preset: 'ts-jest',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
 };
 module.exports = createJestConfig(customJestConfig);
