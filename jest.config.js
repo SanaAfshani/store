a01f1121dbae4ac3c2d3f7183b0706ea260c240b
s.afshani <s.afshani@khodro45.com>
2023-11-30 16:57:24 +0330

    fix: jest issue

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,6 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: [
-    "@testing-library/jest-dom/extend-expect"
-  ],
   preset: 'ts-jest',
 };
 module.exports = createJestConfig(customJestConfig);
